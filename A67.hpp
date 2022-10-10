bf10e2500ea71fa8bcabf3235a3831a664285e6e
Anamay Singh <anaamaysingh@gmail.com>
2022-10-10 16:10:46 -0700

    6.7 assignemtn

--- A67.hpp
+++ A67.hpp
@@ -4,6 +4,44 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &a, int &b){
+    int changer = a;
+    a = b;
+    b = changer; 
+}
+
+void swap(int &a, int &b, int &c){
+    int minimum, maximum, mid;
+    if (a > b && a > c) {
+        a = maximum;
+        mid = (b > c ? b : c);
+        minimum = (b > c ? c : b);
+    }
+    else if (b > a && b > c) {
+        b = maximum;
+        mid = (a > c ? a : c);
+        minimum = (a > c ? b : a);
+
+    } else {
+        c = maximum;
+        mid = (a > b ? a : b);
+        minimum = (a>b ? b : a);
+
+    }
+    a = minimum;
+    b = mid;
+    c = maximum;
+}
+
+void swap (int &a, int &b, int &c, int &d) {
+    int thing = a;
+    a = b;
+    b = c;
+    c = d;
+    d = thing;
+
+}
+
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
